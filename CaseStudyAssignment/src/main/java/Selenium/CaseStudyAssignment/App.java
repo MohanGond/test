56861299fc135d72b507f6ce138e1c9460cf3ec2
MohanGond <mohangond06@gmail.com>
2021-01-07 21:46:19 +0530

    Initial Version

---
+++ CaseStudyAssignment/src/main/java/Selenium/CaseStudyAssignment/App.java
@@ -0,0 +1,13 @@
+package Selenium.CaseStudyAssignment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
